7f9c7d9e43a1f5265390e4f277f79b1d8ba2b3a3
Colton <farlowcolton@yahoo.com>
2018-11-28 23:56:12 -0600

    Update controller-dummy.py

--- controller-dummy.py
+++ controller-dummy.py
@@ -4,7 +4,7 @@ host = socket.gethostname()
 port = 12345                   # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-s.sendall(b'Hello, world')
+s.sendall(b'PLAY')
 data = s.recv(1024)
 s.close()
 print('Received', repr(data))
