8b91a959e70ab37e0343e8947eb4fa383ccfda45
Colton <farlowcolton@yahoo.com>
2018-12-01 18:43:47 -0600

    Update controller-dummy.py

--- controller-dummy.py
+++ controller-dummy.py
@@ -1,10 +1,41 @@
-import socket
-
-host = socket.gethostname()
-port = 12345                   # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-s.sendall(b'PLAY')
-data = s.recv(1024)
-s.close()
-print('Received', repr(data))
+# Import socket module 
+import socket 
+  
+  
+def Main(): 
+    # local host IP '127.0.0.1' 
+    host = '127.0.0.1'
+  
+    # Define the port on which you want to connect 
+    port = 12345
+  
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+  
+    # connect to server on local computer 
+    s.connect((host,port)) 
+  
+    # message you send to server 
+    message = "This is the Controller Speaking"
+    while True: 
+  
+        # message sent to server 
+        s.send(message.encode('ascii')) 
+  
+        # messaga received from server 
+        data = s.recv(1024) 
+  
+        # print the received message 
+        # here it would be a reverse of sent message 
+        print('Received from the the abyss :',str(data.decode('ascii'))) 
+  
+        # ask the client whether he wants to continue 
+        ans = input('\nDo you want to continue connection?(y/n) :') 
+        if ans == 'y': 
+            continue
+        else: 
+            break
+    # close the connection 
+    s.close() 
+  
+if __name__ == '__main__': 
+    Main() 
